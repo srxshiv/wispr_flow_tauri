ab44e1486576ca860131241b2e063fe8e95488c1
srxshiv <srxshiv@gmail.com>
2025-12-27 00:41:59 +0530

    added deepgram connection and basic frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,75 @@
-import { invoke } from "@tauri-apps/api/core"
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { createClient , ListenLiveClient, LiveTranscriptionEvents } from "@deepgram/sdk";
+import useAudioRecorder from "./hooks/useUseAudioRecorder";
 
 export default function App() {
+  const {isRecording , startRecording , stopRecording} = useAudioRecorder();
+  const [transcript , setTranscript] = useState<string>("")
 
-  const [input , setInput] = useState<string>("");
-  const [greet, setGreet] = useState<string>("");
+  const socketRef = useRef<ListenLiveClient | null>(null);
 
-  const handleSubmit = async ()=>{
-    const result = await invoke("greet" , {name: input}) as string;
-    setGreet(result);
+  const handleStartRecording = ()=>{
+    const deepgram = createClient(import.meta.env.VITE_DEEPGRAM_KEY)
+
+    const connection = deepgram.listen.live({
+      model : "nova-2" ,
+      smart_format: true ,
+      language : "en-us"
+    })
+
+    connection.on(LiveTranscriptionEvents.Open , ()=>{
+      console.log("connectes to deepgram")
+
+      startRecording((blob)=>{
+        if(connection.getReadyState()===1){
+          connection.send(blob);
+        }
+      })
+    })
+
+    connection.on(LiveTranscriptionEvents.Transcript , (data)=>{
+      const text = data.channel.alternatives[0].transcript;
+
+      if(text) {
+        setTranscript((prev)=> prev + " " + text)
+      }
+    })
+
+    connection.on(LiveTranscriptionEvents.Close , ()=>{
+      console.log("deepgram connection closedd")
+    })
+
+    socketRef.current = connection;
   }
 
-  return <div>
-    <input value={input} onChange={(e)=>setInput(e.target.value)} />
-    <button onClick={handleSubmit}>Submit</button>
+  const handleStop = () =>{
+    stopRecording();
+    if(socketRef.current){
+      socketRef.current.finish();
+      socketRef.current = null;
+    }
+  }
+
+  return <>
+  <div>
+    <h1>
+      Wispr Flow for Subspace
+    </h1>
+    <div>
+      {isRecording ?
+     ( <button onClick={handleStop}>Stop Recording</button> ): 
+     ( <button onClick={handleStartRecording}>Start Recording</button> )
+      }
+    </div>
 
     <div>
-      {greet}
+      <strong>Transcript : </strong>
+      <div>
+        <p>
+          {transcript}
+        </p>
+      </div>
     </div>
   </div>
+  </>
 }
